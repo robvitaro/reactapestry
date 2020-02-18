56f7a0e8eb39d818442af5b19595fc4d8e372871
robvitaro <robvitaro@gmail.com>
2020-02-18 10:15:18 -0500

    MUCH easier to grok and display hex map

--- src/components/SmallHexMap.js
+++ src/components/SmallHexMap.js
@@ -2,148 +2,46 @@ import React from 'react'
 import {Hexagon, HexGrid, Layout, Text} from "react-hexgrid";
 
 class SmallHexMap extends React.Component {
+  checkUnwantedTiles(q,r,s) {
+    const unwantedTiles = [[-3,0,3],[3,-3,0],[0,3,-3]]
+    let ok = true
+    unwantedTiles.map((tile) => {
+      if(tile[0] === q && tile[1] === r && tile[2] === s) {
+        ok = false
+      }
+    })
+    return ok
+  }
+
   render() {
+
+    const tiles = []
+
+    for (let q = -3; q <= 3; q++) {
+      let r1 = Math.max(-3, -q - 3);
+      let r2 = Math.min(3, -q + 3);
+      for (let r = r1; r <= r2; r++) {
+        let s = -q-r
+        if (this.checkUnwantedTiles(q,r,s)) {
+          let x = q + 3
+          let y = r + 3
+          tiles.push(
+            <Hexagon q={q} r={r} s={s}>
+              <Text className='hidden'>{`${x}, ${y}`}</Text>
+              <Text>{`${q}, ${r}, ${s}`}</Text>
+            </Hexagon>
+          )
+        }
+      }
+    }
+
+
     const hexagonSize = { x: 8, y: 8 };
     return(
       <div>
         <HexGrid width={500} height={300} viewBox="-50 -50 100 100">
           <Layout size={hexagonSize} flat={true} spacing={1.0} origin={{ x: 0, y: 0 }}>
-            <Hexagon q={0} r={0} s={0}>
-              <Text className='hidden'>q, r</Text>
-              <Text>0, 0, 0</Text>
-            </Hexagon>
-            <Hexagon q={0} r={-1} s={1}>
-              <Text className='hidden'>q, r</Text>
-              <Text>0, -1, 1</Text>
-            </Hexagon>
-            <Hexagon q={0} r={1} s={-1}>
-              <Text className='hidden'>q, r</Text>
-              <Text>0, 1, -1</Text>
-            </Hexagon>
-            <Hexagon q={1} r={-1} s={0}>
-              <Text className='hidden'>q, r</Text>
-              <Text>1, -1, 0</Text>
-            </Hexagon>
-            <Hexagon q={1} r={0} s={-1}>
-              <Text className='hidden'>q, r</Text>
-              <Text>1, 0, -1</Text>
-            </Hexagon>
-            <Hexagon q={-1} r={1} s={0}>
-              <Text className='hidden'>q, r</Text>
-              <Text>-1, 1, 0</Text>
-            </Hexagon>
-            <Hexagon q={-1} r={0} s={1}>
-              <Text className='hidden'>q, r</Text>
-              <Text>-1, 0, 1</Text>
-            </Hexagon>
-            <Hexagon q={-2} r={0} s={2}>
-              <Text className='hidden'>q, r</Text>
-              <Text>-2, 0, 2</Text>
-            </Hexagon>
-            <Hexagon q={-2} r={1} s={1}>
-              <Text className='hidden'>q, r</Text>
-              <Text>-2, 1, 1</Text>
-            </Hexagon>
-            <Hexagon q={-2} r={2} s={0}>
-              <Text className='hidden'>q, r</Text>
-              <Text>-2, 2, 0</Text>
-            </Hexagon>
-            <Hexagon q={2} r={-2} s={0}>
-              <Text className='hidden'>q, r</Text>
-              <Text>2, -2, 0</Text>
-            </Hexagon>
-            <Hexagon q={2} r={-1} s={1}>
-              <Text className='hidden'>q, r</Text>
-              <Text>2, -1, 1</Text>
-            </Hexagon>
-            <Hexagon q={2} r={0} s={-2}>
-              <Text className='hidden'>q, r</Text>
-              <Text>2, 0, -2</Text>
-            </Hexagon>
-            <Hexagon q={-1} r={-1} s={2}>
-              <Text className='hidden'>q, r</Text>
-              <Text>-1, -1, 2</Text>
-            </Hexagon>
-            <Hexagon q={0} r={-2} s={2}>
-              <Text className='hidden'>q, r</Text>
-              <Text>0, -2, 2</Text>
-            </Hexagon>
-            <Hexagon q={1} r={-2} s={1}>
-              <Text className='hidden'>q, r</Text>
-              <Text>1, -2, 1</Text>
-            </Hexagon>
-            <Hexagon q={-1} r={2} s={-1}>
-              <Text className='hidden'>q, r</Text>
-              <Text>-1, 2, -1</Text>
-            </Hexagon>
-            <Hexagon q={0} r={2} s={-2}>
-              <Text className='hidden'>q, r</Text>
-              <Text>0, 2, -2</Text>
-            </Hexagon>
-            <Hexagon q={1} r={1} s={-2}>
-              <Text className='hidden'>q, r</Text>
-              <Text>1, 1, -2</Text>
-            </Hexagon>
-            <Hexagon q={-3} r={1} s={2}>
-              <Text className='hidden'>q, r</Text>
-              <Text>-3, 1, 2</Text>
-            </Hexagon>
-            <Hexagon q={-3} r={2} s={1}>
-              <Text className='hidden'>q, r</Text>
-              <Text>-3, 2, 1</Text>
-            </Hexagon>
-            <Hexagon q={-3} r={3} s={0}>
-              <Text className='hidden'>q, r</Text>
-              <Text>-3, 3, 0</Text>
-            </Hexagon>
-            <Hexagon q={3} r={-2} s={-1}>
-              <Text className='hidden'>q, r</Text>
-              <Text>3, -2, -1</Text>
-            </Hexagon>
-            <Hexagon q={3} r={-1} s={-2}>
-              <Text className='hidden'>q, r</Text>
-              <Text>3, -1, -2</Text>
-            </Hexagon>
-            <Hexagon q={3} r={0} s={-3}>
-              <Text className='hidden'>q, r</Text>
-              <Text>3, 0, -3</Text>
-            </Hexagon>
-            <Hexagon q={0} r={-3} s={3}>
-              <Text className='hidden'>q, r</Text>
-              <Text>0, -3, 3</Text>
-            </Hexagon>
-            <Hexagon q={-2} r={-1} s={3}>
-              <Text className='hidden'>q, r</Text>
-              <Text>-2, -1, 3</Text>
-            </Hexagon>
-            <Hexagon q={-1} r={-2} s={3}>
-              <Text className='hidden'>q, r</Text>
-              <Text>-1, -2, 3</Text>
-            </Hexagon>
-            <Hexagon q={1} r={-3} s={2}>
-              <Text className='hidden'>q, r</Text>
-              <Text>1, -3, 2</Text>
-            </Hexagon>
-            <Hexagon q={2} r={-3} s={1}>
-              <Text className='hidden'>q, r</Text>
-              <Text>2, -3, 1</Text>
-            </Hexagon>
-            <Hexagon q={-2} r={3} s={-1}>
-              <Text className='hidden'>q, r</Text>
-              <Text>-2, 3, -1</Text>
-            </Hexagon>
-            <Hexagon q={-1} r={3} s={-2}>
-              <Text className='hidden'>q, r</Text>
-              <Text>-1, 3, -2</Text>
-            </Hexagon>
-            <Hexagon q={1} r={2} s={-3}>
-              <Text className='hidden'>q, r</Text>
-              <Text>1, 2, -3</Text>
-            </Hexagon>
-            <Hexagon q={2} r={1} s={-3}>
-              <Text className='hidden'>q, r</Text>
-              <Text>2, 1, -3</Text>
-            </Hexagon>
+            {tiles}
           </Layout>
         </HexGrid>
       </div>
