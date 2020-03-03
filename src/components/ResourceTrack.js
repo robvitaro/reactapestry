ddf3ad3f4c6c6f22b35638e753c25386b1f53dfb
robvitaro <robvitaro@gmail.com>
2020-03-02 22:11:19 -0500

    Message shows resources required for payment; pointer over icons when paying

--- src/components/ResourceTrack.js
+++ src/components/ResourceTrack.js
@@ -9,20 +9,35 @@ const ResourceTrack = (props) => {
   const {advanceTurnState, resourceChosen} = props
   const maxValue = 8
 
-  const modeMessage = () => {
-    // switch (mode) {
-    //   case 'zeroResources': return <span>You're out of resources. Time to take an income turn?</span>
-    //   case 'chooseResourceAny1': return <span>Choose any 1 resource</span>
-    //   default: return <span>&nbsp;</span>
-    // }
-    return (advanceTurnState && advanceTurnState.state && advanceTurnState.state.matches('PayingCost')) ? <span>Choose any 1 resource</span> : <span>&nbsp;</span>
+  const message = () => {
+    if(advanceTurnState && advanceTurnState.state.matches('PayingCost')) {
+      const cost = advanceTurnState.state.context.advancementCost
+      const wildCount = cost.filter(x => x === 'wild').length
+      const resource = cost.filter(x => x !== 'wild')[0]
+      const resourceCount = cost.filter(x => x !== 'wild').length
+      let message = "To advance, pay "
+
+      if(resource && resourceCount > 0) {
+        message = message.concat(`${resourceCount} ${resource}`)
+      }
+      if(resourceCount > 0 && wildCount > 0) {
+        message = message.concat(' and ')
+      }
+      if(wildCount > 0) {
+        message = message.concat(`${wildCount} of any resource`)
+      }
+      return <span>{message}</span>
+    }
+    return <span>&nbsp;</span>
   }
 
   const takeResource = (resource) => {
-    if (advanceTurnState && advanceTurnState.state && advanceTurnState.state && advanceTurnState.state.matches('PayingCost')) {
+    if (advanceTurnState && advanceTurnState.state.matches('PayingCost')) {
       resourceChosen(resource)
     }
   }
+
+  const payingCostClass = (atIcon) => (atIcon && advanceTurnState && advanceTurnState.state.matches('PayingCost')) ? 'choosing' : ''
 
   const spaces = []
 
@@ -30,18 +45,18 @@ const ResourceTrack = (props) => {
     spaces.push(
       <td key={i}>
         <div>
-          <div className={'resource food'}>
+          <div className={`resource food ${payingCostClass(food === i)}`}>
             {food === i ? <img alt="food" src={foodToken} onClick={()=> takeResource('food')}/> : ''}
           </div>
-          <div className={'resource worker'}>
+          <div className={`resource worker ${payingCostClass(workers === i)}`}>
             {workers === i ? <img alt="worker" src={workerToken} onClick={()=> takeResource('worker')}/> : ''}
           </div>
         </div>
         <div>
-          <div className={'resource coin'}>
+          <div className={`resource coin ${payingCostClass(coin === i)}`}>
             {coin === i ? <img alt="coin" src={coinToken} onClick={()=> takeResource('coin')}/> : ''}
           </div>
-          <div className={'resource culture'}>
+          <div className={`resource culture ${payingCostClass(culture === i)}`}>
             {culture === i ? <img alt="culture" src={cultureToken} onClick={()=> takeResource('culture')}/> : ''}
           </div>
         </div>
@@ -52,7 +67,7 @@ const ResourceTrack = (props) => {
 
   return (
     <div className={'resourceTrack'}>
-      <p className={'smallMessage animated pulse'}>{modeMessage()}</p>
+      <p className={'smallMessage animated pulse'}>{message()}</p>
       <table>
         <tbody>
           <tr>
