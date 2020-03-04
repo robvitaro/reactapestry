266f17512a691077643fd1bab014bcc01f2643c6
robvitaro <robvitaro@gmail.com>
2020-03-04 11:27:14 -0500

    add images to track header rows

--- src/components/TrackHeaderRow.js
+++ src/components/TrackHeaderRow.js
@@ -1,14 +1,49 @@
 import React from 'react';
+import worker from '../img/worker.png';
+import coin from '../img/coin.png';
+import food from '../img/food.png';
+import culture from '../img/culture.png';
+import wild from '../img/wild.png';
 
 const TrackHeaderRow = (props) => {
   const {track} = props
+  const images = {
+    'coin': coin,
+    'culture': culture,
+    'food': food,
+    'workers': worker,
+    'wild': wild
+  }
+
   return (
-    <tr>
-      <td className={track.name.toLowerCase()}><div className='header'>{track.name.toUpperCase()}</div></td>
-      <td colSpan={3}><div>1 Wild</div></td>
-      <td colSpan={3}><div>1 {track.resource}, 1 Wild</div></td>
-      <td colSpan={3}><div>1 {track.resource}, 2 Wild</div></td>
-      <td colSpan={3}><div>2 {track.resource}</div></td>
+    <tr className='trackHeaderRow'>
+      <td className={track.name.toLowerCase()}>
+        <div className='header'>{track.name.toUpperCase()}</div>
+      </td>
+      <td colSpan={3}>
+        <div>
+          <img src={images['wild']} alt={wild} />
+        </div>
+      </td>
+      <td colSpan={3}>
+        <div>
+          <img src={images[track.resource]} alt={track.resource} />
+          <img src={images['wild']} alt={wild} />
+        </div>
+      </td>
+      <td colSpan={3}>
+        <div>
+          <img src={images[track.resource]} alt={track.resource} />
+          <img src={images['wild']} alt={wild} />
+          <img src={images['wild']} alt={wild} />
+        </div>
+      </td>
+      <td colSpan={3}>
+        <div>
+          <img src={images[track.resource]} alt={track.resource} />
+          <img src={images[track.resource]} alt={track.resource} />
+        </div>
+      </td>
     </tr>
   )
 }
