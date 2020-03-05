a2ef879ce5d06b56aeb334e1a324fb45301428f2
robvitaro <robvitaro@gmail.com>
2020-03-05 13:58:36 -0500

    add era headings

--- src/components/TrackHeaderRow.js
+++ src/components/TrackHeaderRow.js
@@ -9,25 +9,33 @@ const TrackHeaderRow = (props) => {
       <td className={track.name.toLowerCase()}>
         <div className='header'>{track.name.toUpperCase()}</div>
       </td>
-      <td colSpan={3}>
+      <td className='era-cell' colSpan={3}>
+        <div className='era-back'/>
+        <div className='era'>I</div>
         <div>
           <img src={IMAGES['wild']} alt={'wild'} />
         </div>
       </td>
-      <td colSpan={3}>
-        <div>
-          <img src={IMAGES[track.resource]} alt={track.resource} />
-          <img src={IMAGES['wild']} alt={'wild'} />
-        </div>
-      </td>
-      <td colSpan={3}>
+      <td className='era-cell' colSpan={3}>
+        <div className='era-back'/>
+        <div className='era'>II</div>
         <div>
           <img src={IMAGES[track.resource]} alt={track.resource} />
           <img src={IMAGES['wild']} alt={'wild'} />
+        </div>
+      </td>
+      <td className='era-cell' colSpan={3}>
+        <div className='era-back'/>
+        <div className='era'>III</div>
+        <div>
+          <img src={IMAGES[track.resource]} alt={track.resource} />
+          <img src={IMAGES['wild']} alt={'wild'} />
           <img src={IMAGES['wild']} alt={'wild'} />
         </div>
       </td>
-      <td colSpan={3}>
+      <td className='era-cell' colSpan={3}>
+        <div className='era-back'/>
+        <div className='era'>IV</div>
         <div>
           <img src={IMAGES[track.resource]} alt={track.resource} />
           <img src={IMAGES[track.resource]} alt={track.resource} />
